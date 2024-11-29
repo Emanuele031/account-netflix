d529d83c9f4ac2866f88701b006c0e605201e913
emanuele <lazzaroemanuele466@gmail.com>
2024-11-29 16:22:09 +0100

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import Navbar from './Component/Navbar';
+import Account from './Component/Account';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-black min-vh-100 d-flex flex-column justify-content-center align-items-center">
+      
+      <Navbar />
+
+     
+      <main className="container text-center">
+        <div className="d-flex justify-content-center align-items-center w-100">
+          <Account />
+        </div>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
